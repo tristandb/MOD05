47c885bbd3cfc8686cdf290d24617c564fa24eaf
Tristan de Boer <tristan@perry.(none)>
2015-10-20 14:20:02 +0000

    Initial picamera is working now

--- picasender/picasender.py
+++ picasender/picasender.py
@@ -3,41 +3,31 @@ import picamera
 import RPi.GPIO as GPIO
 import sys
 
+class CameraWriter(object):
+	# Initialize class
+	color = 0
+	def __init__(self, color):
+		self.color = color
+	def write(self, string):
+		# TODO: Split up strings
+		var = 0
+
 with picamera.PiCamera() as camera:
         # Set color
         if len(sys.argv) > 1:
             if sys.argv[1] == "g":
-                camera = 1
+                color = 1
             elif sys.argv[1] == "b":
-                camera = 2
+                color = 2
             else:
-                camera = 0
+                color = 0
         else:
-            camera = 0
+            color = 0
 
         # Set camera color
-        camera.resolution(1280, 720)
-        setCameraPreferences()
-
-        camera.capture(variable, 'rgb')
-
-        print "%s" % variable
+	
+	cw = CameraWriter(color)
+	 
+        camera.capture(cw, 'rgb')
 
 
-def setCameraPreferences:
-    camera.sharpness = 0
-    camera.contrast = 0
-    camera.brightness = 50
-    camera.saturation = 0
-    camera.ISO = 0
-    camera.video_stabilization = False
-    camera.exposure_compensation = 0
-    camera.exposure_mode = 'auto'
-    camera.meter_mode = 'average'
-    camera.awb_mode = 'auto'
-    camera.image_effect = 'none'
-    camera.color_effects = None
-    camera.rotation = 0
-    camera.hflip = False
-    camera.vflip = False
-    camera.crop = (0.0, 0.0, 1.0, 1.0)
