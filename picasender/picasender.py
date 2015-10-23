bf8c311efc99f1ec398c984c93b7c7a5e59f9c41
Tristan de Boer <tristan@perry.(none)>
2015-10-23 10:11:56 +0000

    First picasender release

--- picasender/picasender.py
+++ picasender/picasender.py
@@ -1,17 +1,60 @@
 import time
+import binascii
 import picamera
 import RPi.GPIO as GPIO
 import sys
+from math import *
+
+# Define ports and clockspeed
+CLOCK_INPUT = 1
+CLOCK_OUTPUT = 2
+NEW_IMAGE_INPUT = 3
+CLOCK_SPEED = 50.0 * 1000.0 * 1000.0
+GPIO_FIRST_OUTPUT = 4
+
+# Set Input/Output ports
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(CLOCK_INPUT, GPIO.IN)
+GPIO.setup(CLOCK_OUTPUT, GPIO.OUT)
+GPIO.setup(NEW_IMAGE_INPUT, GPIO.IN)
+for z in range (0, 8):
+	GPIO.setup(z, GPIO.OUT)
 
 class CameraWriter(object):
-	# Initialize class
+	# Color to search for
 	color = 0
+	# Convert a character to binary
+	def text_to_bits(text, encoding='utf-8', errors='surrogatepass'):
+   	 	bits = bin(int(binascii.hexlify(text.encode(encoding, errors)), 16))[2:]
+    		return bits.zfill(8 * ((len(bits) + 7) // 8))
+	# Initialize class with color
 	def __init__(self, color):
 		self.color = color
+	# Write to GPIO pins
 	def write(self, string):
-		# TODO: Split up strings
-		var = 0
 
+		currentState = 0
+		# Loop string with increments of three
+		for x in range (color, len(string), 3):
+			# while True, used for polling the pins
+			while True:
+				# Check if edge has changed
+				if currentstate != GPIO.input(CLOCK_INPUT):
+					# Set current state to new state
+					currentState = (currentState + 1) % 2
+					toPin = string[x]
+					# Set output pins
+					toPinBinary = text_to_bits(toPin)
+					for y in range (0, 8):
+						GPIO.output(y + GPIO_FIRST_OUTPUT, toPinBinary[y])
+					# Let the FPGA know values have changed
+					GPIO.output(CLOCK_OUTPUT, currentState) 
+					# Break while True
+					break
+				# Sleep for 1/4th of the clock cycle of the FPGA
+				time.sleep(1.0/(float(CLOCK_SPEED)/2.0))
+
+# Start camera
 with picamera.PiCamera() as camera:
         # Set color
         if len(sys.argv) > 1:
@@ -27,7 +70,12 @@ with picamera.PiCamera() as camera:
         # Set camera color
 	
 	cw = CameraWriter(color)
-	 
-        camera.capture(cw, 'rgb')
+	newImage = 0
+	# While loop, keep checking for new image changes
+	while True:
+		# If the FPGA demands a new picture, take a new picture.
+		if newImage != GPIO.input(NEW_IMAGE_INPUT):	
+			camera.capture(cw, 'rgb')
+		time.sleep(1.0/(float(CLOCK_SPEED)/2))
 
 
