fbaff960a55e85e5a9901d2ca31c21495e7a8846
Tristan de Boer <tristandb1@gmail.com>
2015-10-26 09:07:22 +0000

    updated picasender and readme

--- picasender/README.md
+++ picasender/README.md
@@ -6,3 +6,22 @@ When Python is installed, you can install picamera. For instructions please read
 You'll also need RPi.GPIO. For further instructions, please read https://pypi.python.org/pypi/RPi.GPIO.
 
 # Running
+
+# Ports
+Data bit | Data pin
+---------|---------
+7 	 | 29
+6 	 | 31
+5 	 | 32
+4 	 | 33
+3 	 | 35
+2 	 | 36
+1 	 | 37	
+0 	 | 38
+
+Other pins          | Pin
+--------------------|-----
+Request a new image | 18
+Request next data   | 40
+Output new data	    | 22
+
--- picasender/picasender.py
+++ picasender/picasender.py
@@ -7,54 +7,61 @@ from math import *
 
 # Define ports and clockspeed
 # TODO: Set correct pins
-CLOCK_INPUT = 1
-CLOCK_OUTPUT = 2
-NEW_IMAGE_INPUT = 3
-CLOCK_SPEED = 50.0 * 1000.0 * 1000.0
-GPIO_FIRST_OUTPUT = 4
+CLOCK_INPUT = 40
+CLOCK_OUTPUT = 22
+NEW_IMAGE_INPUT = 18
+CLOCK_SPEED = 50.0 * 1000.0
+GPIO_FIRST_OUTPUT = 29
+# 8 pins to send the data over
+GPIO_PIN_ARRAY = [29, 31, 32, 33, 35, 36, 37, 38]
 
+def text_to_bits(text, encoding='utf-8', errors='surrogatepass'):
+	result = bin(ord(text))
+	print result
+        return result[2:]
+
+# Cleanup GPIO
+GPIO.cleanup()
 # Set Input/Output ports
-GPIO.setmode(GPIO.BCM)
+GPIO.setmode(GPIO.BOARD)
 GPIO.setup(CLOCK_INPUT, GPIO.IN)
 GPIO.setup(CLOCK_OUTPUT, GPIO.OUT)
 GPIO.setup(NEW_IMAGE_INPUT, GPIO.IN)
-for z in range (0, 8):
+for z in GPIO_PIN_ARRAY:
 	GPIO.setup(z, GPIO.OUT)
-
-class CameraWriter(object):
+class CameraWriter:
 	# Color to search for
 	color = 0
-	# Convert a character to binary
-	def text_to_bits(text, encoding='utf-8', errors='surrogatepass'):
-   	 	bits = bin(int(binascii.hexlify(text.encode(encoding, errors)), 16))[2:]
-    		return bits.zfill(8 * ((len(bits) + 7) // 8))
 	# Initialize class with color
 	def __init__(self, color):
 		self.color = color
 	# Write to GPIO pins
 	def write(self, string):
-
+		print "Called write object"
 		currentState = 0
 		# Loop string with increments of three
+		print "color: " + str(color) + " stringlen:" + str(len(string))
 		for x in range (color, len(string), 3):
 			# while True, used for polling the pins
 			while True:
 				# Check if edge has changed
-				if currentstate != GPIO.input(CLOCK_INPUT):
+				if currentState != GPIO.input(CLOCK_INPUT):
+					print "sending new"
 					# Set current state to new state
 					currentState = (currentState + 1) % 2
 					toPin = string[x]
 					# Set output pins
 					toPinBinary = text_to_bits(toPin)
-					for y in range (0, 8):
-						GPIO.output(y + GPIO_FIRST_OUTPUT, toPinBinary[y])
+					count = 0
+					for y in GPIO_PIN_ARRAY:
+						print "Output of pin: " + str(y) +  "is set to: " + str(toPinBinary[count])
+						GPIO.output(y, int(toPinBinary[count]))
+						count += 1
 					# Let the FPGA know values have changed
 					GPIO.output(CLOCK_OUTPUT, currentState) 
 					# Break while True
 					break
 				# Sleep for 1/4th of the clock cycle of the FPGA
-				time.sleep(1.0/(float(CLOCK_SPEED)/2.0))
-
 # Start camera
 with picamera.PiCamera() as camera:
         # Set color
@@ -76,8 +83,6 @@ with picamera.PiCamera() as camera:
 	while True:
 		# If the FPGA demands a new picture, take a new picture.
 		if newImage != GPIO.input(NEW_IMAGE_INPUT):	
+			print "Calling writer"
 			newImage = (newImage + 1) % 2
 			camera.capture(cw, 'rgb')
-		time.sleep(1.0/(float(CLOCK_SPEED)/2))
-
-
